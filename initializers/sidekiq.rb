79658e83ef3bc66eb682c7d2b61a6c193850e518
Alexey Katasonov <alexey.kson@gmail.com>
2023-07-06 19:48:38 +0200

    add config for sidekiq

---
+++ initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+require 'sidekiq'
+require 'redis'
+require_relative '../workers/image_downloader'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/0' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/0' }
+end
